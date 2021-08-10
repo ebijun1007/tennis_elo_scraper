63a5380006851b1054ba934791c281290e6f7277
ebijun1007@gmail.com <ebijun1007@gmail.com>
2021-08-10 09:39:41 +0900

    名前の表記揺れ対応

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-import sys
 from datetime import datetime, timedelta, timezone
 
 from bs4 import BeautifulSoup
--- scripts/elo_scraper.py
+++ scripts/elo_scraper.py
@@ -1,7 +1,6 @@
-import os
-import sys
+import json
 
-import requests # to get image from the web
+import requests  # to get image from the web
 from bs4 import BeautifulSoup
 import pandas as pd
 
@@ -12,36 +11,39 @@ class EloScraper:
     link_wta_elo = "https://tennisabstract.com/reports/wta_elo_ratings.html"
     link_wta_yelo = "http://tennisabstract.com/reports/wta_season_yelo_ratings.html"
 
-
     def get_html_table(self, url):
         soup = BeautifulSoup(requests.get(url).content, "lxml")
         return soup.find("table", {"id": "reportable"})
 
-
     def convert_html_table_to_csv(self, table):
         # empty list
         data = []
-        
+        name_list = json.load(open('name_list.json', 'r'))
+
         # for getting the header from the HTML table
         list_header = []
         header = table.find("tr")
-        
+
         for items in header:
             try:
                 list_header.append(items.get_text())
             except:
                 continue
-        
-        # for getting the data 
+
+        # for getting the data
         HTML_data = table.find_all("tr")[1:]
-        
+
         for element in HTML_data:
             sub_data = []
             for sub_element in element:
                 try:
-                    sub_data.append(sub_element.get_text())
+                    sub_data.append(sub_element.get_text(strip=True))
                 except:
                     continue
+            sub_data[1] = sub_data[1].replace('\xa0', ' ')
+            if(sub_data[1] in name_list.keys()):
+                print(sub_data[1])
+                sub_data[1] = name_list[sub_data[1]]
             data.append(sub_data)
 
         return pd.DataFrame(data=data, columns=list_header)
